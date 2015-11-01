60e55c457f24444f57c5086e858936dd2fa4940f
mike <mikew8978@gmail.com>
2015-10-31 20:02:21 -0400

    Set init.d permission

--- libcutils/fs_config.c
+++ libcutils/fs_config.c
@@ -91,6 +91,7 @@ static const struct fs_path_config android_dirs[] = {
     { 00750, AID_ROOT,   AID_SHELL,  0, "sbin" },
     { 00755, AID_ROOT,   AID_ROOT,   0, "system/addon.d" },
     { 00755, AID_ROOT,   AID_SHELL,  0, "system/bin" },
+    { 00755, AID_ROOT,   AID_SHELL,  0, "system/etc/init.d/*" },
     { 00755, AID_ROOT,   AID_SHELL,  0, "system/vendor" },
     { 00755, AID_ROOT,   AID_SHELL,  0, "system/xbin" },
     { 00755, AID_ROOT,   AID_ROOT,   0, "system/etc/ppp" },
